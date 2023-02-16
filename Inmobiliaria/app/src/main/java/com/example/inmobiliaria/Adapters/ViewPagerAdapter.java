16b3b5477f32b12593ea5cb0dba599d26d2d0e71
caguilpa <caguilpa@everis.com>
2023-02-16 13:25:07 +0100

    Commit pildora2

--- Inmobiliaria/app/src/main/java/com/example/inmobiliaria/Adapters/ViewPagerAdapter.java
+++ Inmobiliaria/app/src/main/java/com/example/inmobiliaria/Adapters/ViewPagerAdapter.java
@@ -44,7 +44,8 @@ public class ViewPagerAdapter extends PagerAdapter {
 
         return view == o;
     }
-  
+    /*ERROR*/
+    /*Prueba pildora2*/
     /*Pruebas para gitflow merge conflict eclipse*/
     /*Pruebas para gitflow merge conflict 1*/
     /*Pruebas para gitflow merge conflict 2*/
